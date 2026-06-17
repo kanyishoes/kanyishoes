3380d13a1457676dd0646c7b43348eeb52de3f55
kanyishoes <kanyishoes@proton.me>
2026-06-17 09:00:17 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,29 +1,17 @@
-// Hero Slider
+const swiper = new Swiper(".heroSwiper", {
 
-const heroSwiper = new Swiper(".heroSwiper", {
-    loop: true,
+loop: true,
 
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
+autoplay: {
+delay: 4000,
+disableOnInteraction: false
+},
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+pagination: {
+el: ".swiper-pagination",
+clickable: true
+}
+
 });
 
-// Demo Counts
-
-let cartCount = 3;
-let favoriteCount = 2;
-
-// Future Firebase Integration
-// These values will come from Firestore
-
-console.log("KANYI Shoes Home Loaded");
-
-// Install PWA Prompt Placeholder
-
-window.addEventListener("load", ()
+console.log("KANYI SHOES HOME READY");
